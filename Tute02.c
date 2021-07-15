f2fef8339ae15a34742afe980d1062914680f6ba
it21128936 <87465484+it21128936@users.noreply.github.com>
2021-07-15 07:10:02 +0000

    Done exercise 2

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int dis;
+  float tot,amount1,amount2;
+  printf("Enter distance :");
+  scanf("%d",&dis);
+  if(dis<=30)
+  {
+    tot = dis*50.0;
+    printf("Total Amount : %.2f",tot);
+  }
+  else
+  {
+    amount1 = 30*50.0;
+    amount2 = (dis-30)*40.0;
+    tot = amount1 + amount2;
+    printf("Total Amount : %.2f",tot);
+  }
   
   return 0;
 }
