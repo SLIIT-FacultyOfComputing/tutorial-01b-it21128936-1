19b6ce6a281e771317e33a67a7e65b6a65f00aa5
it21128936 <87465484+it21128936@users.noreply.github.com>
2021-07-15 07:01:45 +0000

    Done exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2,tot;
+  float avg;
+  printf("Enter marks for subject 1 :");
+  scanf("%d",&mark1);
+  printf("Enter marks for subject 2 :");
+  scanf("%d",&mark2);
+  tot = mark1 + mark2;
+  avg = tot/2.0;
+  
+  printf("Average : %.2f",avg);
   
   return 0;
 }
